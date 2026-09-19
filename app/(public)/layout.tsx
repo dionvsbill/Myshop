5160e59f31eb88d96aee9e586f924fc12ddaeb25
dionvsbill <ndw9pwzscv@privaterelay.appleid.com>
2026-09-19 12:35:23 +0000

    Improve public storefront shell

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,14 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
-export default function PublicLayout({children}:{children:React.ReactNode}){
-  return <><Header/><main className="container py-6">{children}</main><Footer/></>
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-[60vh]">
+        <div className="container py-8">{children}</div>
+      </main>
+      <Footer />
+    </>
+  );
 }
