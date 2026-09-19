9a9f75f582062b38739e3fceb800e7e7d9ec3fad
dionvsbill <ndw9pwzscv@privaterelay.appleid.com>
2026-09-19 13:35:20 +0000

    apply CSS Modules app shell

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,4 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import MobileBottomNav from "../../components/MobileBottomNav";
-export default function PublicLayout({children}:{children:React.ReactNode}){return <><Header/><main className="min-h-[60vh]"><div className="container py-5 pb-24">{children}</div></main><Footer/><MobileBottomNav/></>}
+export default function PublicLayout({children}:{children:React.ReactNode}){return <><Header/><main className="page"><div className="mainContent">{children}</div></main><Footer/><MobileBottomNav/></>}
