e30969b691fb4d60eed65151af3ee07383ba632f
dionvsbill <ndw9pwzscv@privaterelay.appleid.com>
2026-09-19 11:10:09 +0000

    Add auth callback and reusable UI

---
+++ components/ui/button.tsx
@@ -0,0 +1,1 @@
+import * as React from "react"; import {cn} from "@/lib/utils"; export function Button({className,...props}:React.ButtonHTMLAttributes<HTMLButtonElement>){return <button className={cn("inline-flex items-center justify-center rounded-lg px-4 py-2 font-medium transition disabled:opacity-50",className)} {...props}/>}
